c3021244cc72f3a65403abded13e31521b87f6f1
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2021-05-01 18:56:16 +0300

    Recipe switch basic implementation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,18 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  @Output() recipeShow = new EventEmitter<{ recipeShow: boolean }>();
+  @Output() listShow = new EventEmitter<{ listShow: boolean }>();
 
+  onRecipeClick(): void {
+    this.recipeShow.emit({recipeShow: true});
+  }
+
+  onListClick(): void {
+    this.listShow.emit({listShow: true});
+  }
 }
