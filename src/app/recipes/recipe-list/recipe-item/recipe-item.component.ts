5d223cf3df8ac38de97a733e3286f03c2d631bc9
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2021-05-25 07:53:46 +0300

    Added functionality to select recipe.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Recipe} from '../../recipe.model';
 
 @Component({
@@ -7,6 +7,12 @@ import {Recipe} from '../../recipe.model';
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe = new Recipe('', '', '');
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   ngOnInit(): void {
+  }
+
+  onSelect(): void{
+    this.recipeSelected.emit(this.recipe);
   }
 }
